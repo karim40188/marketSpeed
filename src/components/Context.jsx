a17adefbeca68aeb5734898905fd5b93be71c133
karim mohamed <karim40189@gmail.com>
2024-10-27 11:33:22 +0200

    some edit

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -4,7 +4,7 @@ export let Context = createContext("");
 
 // eslint-disable-next-line react/prop-types
 export function ContextProvider({ children }) {
-  let [sidebarOpen, setSidebarOpen] = useState(true);
+  let [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <Context.Provider value={{ sidebarOpen, setSidebarOpen }}>
       {children}
