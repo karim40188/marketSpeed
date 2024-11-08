ec2e9f44ae07e98c45cacd87a4d5d4653dda6155
karim mohamed <karim40189@gmail.com>
2024-11-08 19:02:22 +0200

    some edit

--- src/components/ShowSupervisor.jsx
+++ src/components/ShowSupervisor.jsx
@@ -30,7 +30,7 @@ function ShowSupervisor() {
         <Typography
           sx={{ fontSize: { xs: "30px", md: "40px" }, color: "#114F80",fontWeight:'600' }}
         >
-          عرض المشرفين
+          عرض المسؤلين
         </Typography>
         <Box sx={{ position: "relative" }}>
           <TextField
